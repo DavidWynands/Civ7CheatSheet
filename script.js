593ed76de7c2dbc4168a55bc4562973483901b43
David Wynands <davidwynands@web.de>
2025-02-15 17:12:54 +0100

    added first table prototype with icon

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Filter for the list
 document.getElementById('filterInput').addEventListener('input', function() {
     var filterValue = this.value.toLowerCase();
     var listItems = document.querySelectorAll('.list-item');
@@ -7,6 +8,20 @@ document.getElementById('filterInput').addEventListener('input', function() {
             item.style.display = '';
         } else {
             item.style.display = 'none';
+        }
+    });
+});
+
+// Filter for the table
+document.getElementById('filterInputTable').addEventListener('input', function() {
+    var filterValue = this.value.toLowerCase();
+    var tableRows = document.querySelectorAll('.table-row');
+
+    tableRows.forEach(function(row) {
+        if (row.textContent.toLowerCase().includes(filterValue)) {
+            row.style.display = '';
+        } else {
+            row.style.display = 'none';
         }
     });
 });
